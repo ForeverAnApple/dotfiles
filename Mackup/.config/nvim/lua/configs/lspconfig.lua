e036c9fdee1357e023aa66145348fe9a122d8f39
ForeverAnApple <kinbd8@gmail.com>
2024-03-14 23:53:06 -0700

    Adding special configurations for certain lsp

--- Mackup/.config/nvim/lua/configs/lspconfig.lua
+++ Mackup/.config/nvim/lua/configs/lspconfig.lua
@@ -6,11 +6,26 @@ local capabilities = configs.capabilities
 
 local lspconfig = require "lspconfig"
 local servers = { "html", "cssls", "clangd", "rust_analyzer" }
+local special_configs = {
+  rust_analyzer = {
+    filetypes = {"rust"},
+    root_dir = lspconfig.util.root_pattern("Cargo.toml")
+  }
+}
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  local basic = {
     on_init = on_init,
     on_attach = on_attach,
     capabilities = capabilities,
   }
+
+  if special_configs[lsp] ~= nil then
+    lspconfig[lsp].setup(
+      vim.tbl_deep_extend("force", basic, special_configs[lsp])
+    )
+  else
+    lspconfig[lsp].setup(basic)
+  end
 end
+
