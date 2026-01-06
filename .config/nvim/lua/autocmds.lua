c7646311d0f6c620d8b2676d5104ca75ffa0d7b6
ForeverAnApple <kinbd8@gmail.com>
2026-01-06 11:31:50 -0800

    Updated nvim to use opencode and some additional changes

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -7,3 +7,8 @@ vim.api.nvim_create_autocmd("VimEnter", {
     vim.cmd("wincmd p")  -- Focus back to the editing buffer
   end,
 })
+
+-- Dim gitignored files in nvim-tree
+vim.api.nvim_set_hl(0, "NvimTreeGitIgnoredIcon", { fg = "#51576d" })
+vim.api.nvim_set_hl(0, "NvimTreeGitFileIgnoredHL", { fg = "#51576d" })
+vim.api.nvim_set_hl(0, "NvimTreeGitFolderIgnoredHL", { fg = "#51576d" })
